1f8611feca48ed41f0d9499c99ec2de5b348a587
Niels Kübler <niels.kuebler@uzh.ch>
2021-03-30 20:14:30 +0200

    Test cleanup

--- eloquent-elks-poi-api/src/main/java/eloquentelks/poi/api/model/PoiGetDto.java
+++ eloquent-elks-poi-api/src/main/java/eloquentelks/poi/api/model/PoiGetDto.java
@@ -3,7 +3,6 @@ package eloquentelks.poi.api.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
