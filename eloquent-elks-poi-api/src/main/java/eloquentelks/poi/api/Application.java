3fb00530dfac2eedbd57e3362d2cad84f5baebfb
Niels Kübler <niels.kuebler@uzh.ch>
2021-04-01 17:16:56 +0200

    Docker configuration for spring boot application

--- eloquent-elks-poi-api/src/main/java/eloquentelks/poi/api/Application.java
+++ eloquent-elks-poi-api/src/main/java/eloquentelks/poi/api/Application.java
@@ -1,16 +1,11 @@
 package eloquentelks.poi.api;
 
-
 import eloquentelks.poi.api.repository.FeatureRepository;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-/**
- * Main entry point to start the application
- */
-@EnableMongoRepositories(basePackageClasses = {FeatureRepository.class})
+@EnableMongoRepositories(basePackageClasses = FeatureRepository.class)
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
