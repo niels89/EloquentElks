742181faedf0bd9e830058b4bcf605d665ba8f49
Prasun Saurabh <prasun.saurabh@uzh.ch>
2021-04-06 09:32:44 +0200

    Resolving rename error

--- eloquent-elks-backend/src/test/java/com/airbnb/eloquentelksbackend/DTO/PropertyFetchDTOTest.java
+++ eloquent-elks-backend/src/test/java/com/airbnb/eloquentelksbackend/DTO/PropertyFetchDTOTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class PropertyFecthDTOTest {
+public class PropertyFetchDTOTest {
 
     @Test
     public void testPropertyGetDTO(){
