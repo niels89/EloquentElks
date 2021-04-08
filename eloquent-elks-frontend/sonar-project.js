640a32a143138a3e4207d44ce1b0c4b5ab9b8999
Niels Kübler <niels.kuebler@uzh.ch>
2021-04-08 10:02:04 +0200

    Configured sonar to read token from environment variable for frontend analysis.

--- eloquent-elks-frontend/sonar-project.js
+++ eloquent-elks-frontend/sonar-project.js
@@ -3,11 +3,13 @@ const sonarScanner = require("sonarqube-scanner");
 sonarScanner(
     {
         serverUrl: "https://sonarcloud.io",
+        token: process.env.SONAR_TOKEN,
         options:{
             "sonar.projectKey": "eloquent-elks_EloquentElks_Frontend",
             "sonar.organization": "eloquent-elks",
             "sonar.sources": "src",
             "sonar.tests": "src",
+            "sonar.test.inclusions": "src/*.test.js,src/*Tests.js",
             "sonar.typescript.lcov.reportPaths": "coverage/lcov.info"
         }
     }
