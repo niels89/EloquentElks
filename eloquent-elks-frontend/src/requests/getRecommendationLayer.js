bee097175b5b9399e2ae2166a34767bcf41416f1
Patrick Luchsinger <patrick.lu@bluewin.ch>
2021-04-27 11:28:44 +0200

    Changed dbseed for pois with improved data and added scripts to process the geodata from osm

--- eloquent-elks-frontend/src/requests/getRecommendationLayer.js
+++ eloquent-elks-frontend/src/requests/getRecommendationLayer.js
@@ -8,7 +8,7 @@ import {getVersion} from "../helpers/getVersion";
 export async function getRecommendationLayer(attractionTypes) {
     let response = await fetch(`${getDomain(1338)}/api/${getVersion()}/recommendation/area`,
         {method: 'POST',
-            body: JSON.stringify({"attractionTypes": ["museum", "attraction"]}),
+            body: JSON.stringify({"attractionTypes": ["salon"]}),
             headers: {'Content-Type': 'application/json'}
         })
     if (response.ok) {
