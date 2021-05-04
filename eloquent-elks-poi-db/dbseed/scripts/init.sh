06efb21cdbf9113239e1cb05bb06a499d4341843
Niels Kübler <niels.kuebler@uzh.ch>
2021-05-04 10:37:12 +0200

    Added famous POIs to POI database

--- eloquent-elks-poi-db/dbseed/scripts/init.sh
+++ eloquent-elks-poi-db/dbseed/scripts/init.sh
@@ -1,1 +1,4 @@
-mongoimport --host mongodb_poi --db eloquent-elks --collection feature --type json --file /poi.json --jsonArray
+#!/bin/bash
+
+mongoimport --host mongodb_poi --db eloquent-elks --collection feature --type json --file /poi.json --jsonArray && 
+mongoimport --host mongodb_poi --db eloquent-elks --collection feature --type json --file /famous.json --jsonArray
